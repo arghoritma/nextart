94ad85981f07fa08962b3b847421d297ddb9bcc3
Abdul Rojak <bigzero55@outlook.com>
2025-04-05 17:33:32 +0700

    final release-pg

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB84rVbpDXXSxj04pLa0FJ23zIbCriiVDs",
-  authDomain: "nextart-arghoritma.firebaseapp.com",
-  projectId: "nextart-arghoritma",
-  storageBucket: "nextart-arghoritma.firebasestorage.app",
-  messagingSenderId: "209111306229",
-  appId: "1:209111306229:web:58bc478588b7a5f6fb9abc",
-  measurementId: "G-PVME17HBYX",
+  apiKey: "YOUR-API-KEY",
+  authDomain: "YOUR-AUTH-DOMAIN",
+  projectId: "YOUR-PROJECT-ID",
+  storageBucket: "YOUR-STORAGE-BUCKET",
+  messagingSenderId: "YOUR-MESSAGING-SENDER-ID",
+  appId: "YOUR-APP-ID",
+  measurementId: "YOUR-MEASUREMENT-ID",
 };
 
 // Initialize Firebase
