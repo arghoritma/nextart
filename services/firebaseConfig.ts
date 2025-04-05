acfaf935806a2a93aa0c974f237abfc90810b159
Abdul Rojak <bigzero55@outlook.com>
2025-04-05 17:30:53 +0700

    final-release-mysql

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB84rVbpDXXSxj04pLa0FJ23zIbCriiVDs",
-  authDomain: "nextart-arghoritma.firebaseapp.com",
-  projectId: "nextart-arghoritma",
-  storageBucket: "nextart-arghoritma.firebasestorage.app",
-  messagingSenderId: "209111306229",
-  appId: "1:209111306229:web:58bc478588b7a5f6fb9abc",
-  measurementId: "G-PVME17HBYX",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID",
 };
 
 // Initialize Firebase
