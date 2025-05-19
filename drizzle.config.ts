57cd2924ca9382c5f0dfd0a85549076cbed4ecf8
Abdul Rojak <bigzero55@outlook.com>
2025-05-19 11:25:40 +0700

    update: name db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "drizzle-kit";
 import "dotenv/config";
 
+const DB_NAME = process.env.NODE_ENV === "development" ? process.env.DB_NAME : "prod";
 export default defineConfig({
   out: "./migrations",
   schema: "./schemas/*.ts",
   dialect: "sqlite",
   dbCredentials: {
-    url: `${process.env.DATABASE_PATH}/${process.env.NODE_ENV}.db`,
+    url: `${process.env.DATABASE_PATH}/${DB_NAME}.db`,
   },
   verbose: true,
 });
